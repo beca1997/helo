969d0176a7d9a44f192f5e4b320ccc459ad4ded8
Giacomo Becatti <becattigiacomo@gmail.com>
2021-03-15 10:23:58 +0100

    first commit

---
+++ hello.cpp
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello.cpp
+ *
+ *    Description:  helo
+ *
+ *        Version:  1.0
+ *        Created:  15/03/2021 09:45:26
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Giacomo Becatti (gb), giacomo.becatti@studenti.unimi.it
+ *        Company:  Università degli studi di Milano, UNIMI
+ *
+ * =====================================================================================
+ */
+#include<iostream>
+using namespace std;
+
+
+	int
+main ( int argc, char *argv[] )
+{
+	cout << "Hello, world!" << endl;
+	return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
+
